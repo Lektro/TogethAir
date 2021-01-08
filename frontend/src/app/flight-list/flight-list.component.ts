f2c541557459a7d467e1bdb666621f6a36132234
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-08 12:37:42 +0100

    two restcontrollers now seeding one view. build on this model

--- frontend/src/app/flight-list/flight-list.component.ts
+++ frontend/src/app/flight-list/flight-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../model/flight';
+import { Airport } from '../model/airport';
 import { FlightService } from '../service/flight.service';
+import { AirportService} from "../service/airport.service";
 
 @Component({
   selector: 'app-flight-list',
@@ -11,12 +13,18 @@ import { FlightService } from '../service/flight.service';
 export class FlightListComponent implements OnInit {
 
   flights : Flight[] =[];
+  airports: Airport[] = [];
 
-  constructor(private flightService: FlightService) { }
+  constructor(private flightService: FlightService, private airportsService: AirportService) { }
 
   ngOnInit() {
-    this.flightService.findAll().subscribe((data: any) => { this.flights = data;
+    this.flightService.findAll().subscribe((flightData: any) => {
+      this.flights = flightData
     });
-  }
+    this.airportsService.findAll().subscribe((airportData: any) => {
+      this.airports = airportData
+    });
 
+
+  }
 }
--- frontend/src/app/service/flight.service.ts
+++ frontend/src/app/service/flight.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Flight } from '../model/flight';
 import { Observable } from 'rxjs';
+import {Airport} from "../model/airport";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
-    private flightsUrl = 'http://localhost:8080/api/flights';
-
-    constructor(private http: HttpClient) {
-    }
+    private readonly flightsUrl = 'http://localhost:8080/api/flights';
 
     public findAll() {
       return this.http.get(this.flightsUrl);
@@ -19,4 +17,15 @@ export class FlightService {
     public save(flight: Flight) {
       return this.http.post<Flight>(this.flightsUrl, flight);
     }
+
+  private readonly airportUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.airportUrl = 'http://localhost:8080/api/airports';
+    this.flightsUrl = 'http://localhost:8080/api/flights';
+  }
+
+  public findAllAirports(): Observable<Airport[]> {
+    return this.http.get<Airport[]>(this.airportUrl);
+  }
 }

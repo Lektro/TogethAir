d38166d70739a621263c13b574bde87a5b78dddb
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2021-01-11 17:08:11 +0100

    dropdown works in add flight, error messages behave correctly

--- frontend/src/app/flight-form/flight-form.component.ts
+++ frontend/src/app/flight-form/flight-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FlightService } from '../service/flight.service';
-import { Flight } from '../model/flight';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FlightService} from '../service/flight.service';
+import {Flight} from '../model/flight';
 import {Airport} from "../model/airport";
+import {AirportService} from "../service/airport.service";
 
 @Component({
   selector: 'app-flight-form',
@@ -11,15 +12,16 @@ import {Airport} from "../model/airport";
 })
 export class FlightFormComponent implements OnInit {
 
-  flight : Flight;
-  flights : Flight[] =[];
+  flight: Flight;
+  flights: Flight[] = [];
   airports: Airport[] = [];
 
   constructor(
-      private route: ActivatedRoute,
-          private router: Router,
-              private flightService: FlightService) {
-      this.flight = new Flight();
+    private route: ActivatedRoute,
+    private router: Router,
+    private flightService: FlightService,
+    private airportService: AirportService) {
+    this.flight = new Flight();
   }
 
   onSubmit() {
@@ -31,6 +33,7 @@ export class FlightFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.airportService.findAll().subscribe(result => this.airports = result)
   }
 
 }
